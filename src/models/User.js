9ceca5f0448f7e5bd785304316f14bd42d7daecd
Sanyam26362 <agarwalsanyam150@gmail.com>
2025-10-03 08:43:48 +0530

    Timeseries changed to timestamps

--- src/models/User.js
+++ src/models/User.js
@@ -4,5 +4,5 @@ const userSchema = new mongoose.Schema({
     email:{type:String,required:true,unique:true,lowercase:true},
     passwordHash:{type:String,required:true},
     role:{type:String,enum:["user","admin"],default:"user"}
-},{timeseries:true});
+},{timestamps:true});
 module.exports= mongoose.model("User",userSchema);
