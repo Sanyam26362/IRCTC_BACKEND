933ac92eabc7cf123445cf6a8a8567d2446a7a13
Sanyam26362 <agarwalsanyam150@gmail.com>
2025-10-03 08:40:54 +0530

    Index and User

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,11 @@
 const mongoose = require("mongoose");
-const connectionDB = async() => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI),
-        console.log(`MONGODB CONNECTED: ${connectionDB.connection.host}`);
-        } catch(error) {
-            console.error("MONGODB CONNECTION FAILED",error.message);
-            process.exit(1);
-
-        }
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.error("MongoDB connection failed:", err.message);
+    process.exit(1);
+  }
 };
-module.exports= connectionDB;
+module.exports = connectDB;
